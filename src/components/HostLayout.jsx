eb3034016fab4e2d062ccda49fce76d705361fb7
Freddie <119540226+SanJammin@users.noreply.github.com>
2025-06-15 17:30:54 +0100

    Update HostLayout.jsx

--- src/components/HostLayout.jsx
+++ src/components/HostLayout.jsx
@@ -1,13 +1,14 @@
-import { Link } from "react-router-dom";
+import { Link, Outlet } from "react-router-dom";
 
 export default function HostLayout() {
     return (
-        <section>
-            <nav>
+        <>
+            <nav className="host-nav">
             <Link to="/host">Dashboard</Link>
             <Link to="/host/income">Income</Link>
             <Link to="/host/reviews">Reviews</Link>
             </nav>
-        </section>
+            <Outlet />
+        </>
     );
 }
