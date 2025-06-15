4b0d1c81fb9a673ee65fb60146b1f5a42ff7e836
Freddie <119540226+SanJammin@users.noreply.github.com>
2025-06-15 11:25:03 +0100

    Create home and about page

---
+++ README.md
@@ -0,0 +1,3 @@
+## Figma
+
+https://www.figma.com/design/QQ8lqlpHADHbOTMU2tQwvj/-VanLife--Copy-?node-id=0-1&p=f&t=5thMhLoHpsVfg6b3-0
--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,18 @@
 import { createRoot } from "react-dom/client";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import "./index.css";
 
 function App() {
   return (
     <BrowserRouter>
+      <header>
+        <Link className="site-logo" to="/">#VanLife</Link>
+        <nav>
+          <Link to="/about">About</Link>
+        </nav>
+      </header>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
