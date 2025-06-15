612a9af72b1ef2d281ddd0574260239816cc71d7
Freddie <119540226+SanJammin@users.noreply.github.com>
2025-06-15 17:24:03 +0100

    Create the Host Layout

---
+++ src/components/HostLayout.jsx
@@ -0,0 +1,13 @@
+import { Link } from "react-router-dom";
+
+export default function HostLayout() {
+    return (
+        <section>
+            <nav>
+            <Link to="/host">Dashboard</Link>
+            <Link to="/host/income">Income</Link>
+            <Link to="/host/reviews">Reviews</Link>
+            </nav>
+        </section>
+    );
+}
--- src/main.jsx
+++ src/main.jsx
@@ -5,10 +5,11 @@ import Home from "./pages/Home";
 import About from "./pages/About";
 import Vans from "./pages/Vans/Vans";
 import VanDetail from "./pages/Vans/VanDetail";
-import Layout from "./components/Layout";
 import Dashboard from "./pages/Host/Dashboard";
 import Income from "./pages/Host/Income";
 import Reviews from "./pages/Host/Reviews";
+import Layout from "./components/Layout";
+import HostLayout from "./components/HostLayout";
 
 import "../server";
 
@@ -21,9 +22,11 @@ function App() {
           <Route path="/about" element={<About />} />
           <Route path="/vans" element={<Vans />} />
           <Route path="/vans/:id" element={<VanDetail />} />
-          <Route path="/host" element={<Dashboard />} />
-          <Route path="/host/income" element={<Income />} />
-          <Route path="/host/reviews" element={<Reviews />} />
+
+          <Route path="/host" element={<HostLayout />}>
+            <Route path="/host/income" element={<Income />} />
+            <Route path="/host/reviews" element={<Reviews />} />
+          </Route>
         </Route>
       </Routes>
     </BrowserRouter>
