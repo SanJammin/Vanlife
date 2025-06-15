974558f989ee55087c5bebc9a7c523e1a9c5ab64
Freddie <119540226+SanJammin@users.noreply.github.com>
2025-06-15 18:15:23 +0100

    Update Header to use NavLink

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,13 +1,34 @@
-import { Link } from "react-router-dom";
+import { Link, NavLink } from "react-router-dom";
 
 export default function Header() {
+    const activeLink = {
+        fontWeight: "bold",
+        textDecoration: "underline",
+        color: "#161616"
+    }
+
     return (
         <header>
             <Link className="site-logo" to="/">#VanLife</Link>
             <nav>
-            <Link to="/host">Host</Link>
-            <Link to="/about">About</Link>
-            <Link to="/vans">Vans</Link>
+            <NavLink 
+                to="/host"
+                style={({isActive}) => isActive ? activeLink : null}
+            >
+                Host
+            </NavLink>
+            <NavLink 
+                to="/about"
+                style={({isActive}) => isActive ? activeLink : null}
+            >
+                About
+            </NavLink>
+            <NavLink 
+                to="/vans"
+                style={({isActive}) => isActive ? activeLink : null}
+            >
+                Vans
+            </NavLink>
             </nav>
         </header>
     );
